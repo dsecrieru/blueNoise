939ec524eabba5ebcc89fb96009569290ebf5212
Daniel Secrieru <danysecrieru@yahoo.com>
2023-03-18 11:56:14 +0100

    fixed bugs; refactoring; still has some debug/profiling statements in

--- pds_Bridson.hpp
+++ pds_Bridson.hpp
@@ -5,83 +5,88 @@
 #include <vector>
 #include <random>
 #include <cmath>
+
 #include <iostream>
+#include <chrono>
 
 namespace blue_noise {
 
-template<typename point_t>
-struct _grid_t {
-    typedef std::vector<point_t> points_t;
-    typedef points_t::size_type pos_t;
-    typedef std::vector<bool> valid_t;
+typedef uint16_t int_t;
+typedef float float_t;
 
-    float cell_size;
-    pos_t w;
-    pos_t h;
-    points_t points;
-    valid_t valid;
+template<typename point2d_t>
+class _grid_t {
+public:
+    typedef std::vector<point2d_t> points_t;
+    typedef points_t::size_type size_type;
+
+    size_type w, h;
+    int_t orig_w, orig_h;
+
+public:
+    _grid_t() = delete;
+    _grid_t(int_t width, int_t height, float_t cell_size) : orig_w(width), orig_h(height), _cell_size(cell_size) {
+        w = std::lround(float_t(orig_w) / _cell_size);
+        h = std::lround(float_t(orig_h) / _cell_size);
+        
+        _points.resize(w * h);
+        _has_point.resize(w * h, false);
+    }
 
     struct coord_t {
-        pos_t x, y;
+        size_type x, y;
         coord_t() = delete;
-        coord_t(pos_t x_coord, pos_t y_coord) : x(x_coord), y(y_coord) {}
+        coord_t(size_type x_coord, size_type y_coord) : x(x_coord), y(y_coord) {}
     };
 
-    _grid_t() = delete;
-    _grid_t(float cs, pos_t gw, pos_t gh) : cell_size(cs), w(gw), h(gh), points(w * h), valid(w * h, false) {}
-
-    coord_t cell_coord(const point_t& point) const {
-        return coord_t(std::min(static_cast<pos_t>(point.x / cell_size), w - 1),
-                       std::min(static_cast<pos_t>(point.y / cell_size), h - 1));
+    coord_t cell_coord(const point2d_t& point) const {
+        return coord_t(std::min(static_cast<size_type>(point.x / _cell_size), w - 1),
+                       std::min(static_cast<size_type>(point.y / _cell_size), h - 1));
     }
 
-    void add_point(const point_t& point) {
-        static int counter = 1;
+    void add_point(const point2d_t& point) {
         auto coord = cell_coord(point);
-        points[coord.y * w + coord.x] = point;
-        valid[coord.y * w + coord.x] = true;
-        ++counter;
-        std::cout << counter << std::endl;
+        _points[coord.y * w + coord.x] = point;
+        _has_point[coord.y * w + coord.x] = true;
     }
 
-    bool valid_at(pos_t grid_x, pos_t grid_y) const {
-        return valid[grid_y * w + grid_x];
+    bool has_point_at(size_type grid_x, size_type grid_y) const {
+        return _has_point[grid_y * w + grid_x];
     }
 
-    float distance(pos_t grid_x, pos_t grid_y, const point_t& point) const {
-        auto dx = point.x - points[grid_y * w + grid_x].x;
-        auto dy = point.y - points[grid_y * w + grid_x].y;
+    float_t distance(size_type grid_x, size_type grid_y, const point2d_t& point) const {
+        auto dx = point.x - _points[grid_y * w + grid_x].x;
+        auto dy = point.y - _points[grid_y * w + grid_x].y;
         return std::sqrt(dx * dx + dy * dy);
     }
+
+private:
+    float_t _cell_size;
+    points_t _points;
+    std::vector<bool> _has_point;
 };
 
 template<typename T>
-T _remove_at(std::vector<T>& vec, typename std::vector<T>::size_type pos) {
-	T elem = std::move_if_noexcept(vec[pos]);
+void _remove_at(std::vector<T>& vec, typename std::vector<T>::size_type pos) {
 	vec[pos] = std::move_if_noexcept((vec.back()));
 	vec.pop_back();
-	return elem;
 }
 
-template<typename T, typename rn_engine_t>
-T _remove_random(std::vector<T>& vec, rn_engine_t& rne) {
-    std::uniform_int_distribution<typename std::vector<T>::size_type> rng(0, vec.size() - 1);
-
-    return _remove_at(vec, rng(rne));
-}
-
-template<typename point_t, typename rn_engine_t>
-point_t _generate_random_point_around(const point_t& point, float min_dist, rn_engine_t& rne) {
-    std::uniform_real_distribution<float> rng;
+template<typename point2d_t, typename rn_engine_t>
+point2d_t _generate_random_point_around(const point2d_t& point, float_t min_dist, rn_engine_t& rne) {
+    std::uniform_real_distribution<float_t> rng;
 
     auto radius = min_dist * (rng(rne) + 1.0f);
-    auto angle = 2.0f * std::numbers::pi_v<float> * rng(rne);
+    auto angle_radians = 2.0f * std::numbers::pi_v<float_t> * rng(rne);
 
-    return point_t(point.x + radius * std::cos(angle), point.y + radius * sin(angle));
+    return point2d_t(point.x + radius * std::cos(angle_radians), point.y + radius * sin(angle_radians));
 }
 
-template<typename point_t>
-bool _is_valid_candidate(const point_t& point, const _grid_t<point_t>& grid, float min_dist) {
+template<typename point2d_t>
+bool _is_valid_candidate(const point2d_t& point, const _grid_t<point2d_t>& grid, float_t min_dist) {
+    if (point.x < 0 || point.x >= grid.orig_w || point.y < 0 || point.y >= grid.orig_h)
+        return false;
+
     auto grid_coord = grid.cell_coord(point);
 
     auto start_x = grid_coord.x >= 2 ? grid_coord.x - 2 : 0;
@@ -91,7 +96,7 @@ bool _is_valid_candidate(const point_t& point, const _grid_t<point_t>& grid, flo
 
     for (int i = start_x; i <= end_x; ++i)
         for (int j = start_y; j <= end_y; ++j) {
-            if (grid.valid_at(i, j) && grid.distance(i, j, point) < min_dist - FLT_EPSILON)
+            if (grid.has_point_at(i, j) && grid.distance(i, j, point) < min_dist - FLT_EPSILON)
                 return false;
         }
 
@@ -101,44 +106,55 @@ bool _is_valid_candidate(const point_t& point, const _grid_t<point_t>& grid, flo
 /*
  *
  */
-template<typename point_t, typename rn_engine_t>
-std::vector<point_t> pds_Bridson_sampling_2d(uint16_t width, uint16_t height, float min_dist, uint8_t k_max_point_count, rn_engine_t& rne) {
-	std::uniform_real_distribution<float> rng;
+template<typename point2d_t, typename rn_engine_t>
+std::vector<point2d_t> pds_Bridson_sampling_2d(int_t width, int_t height, float_t min_dist, int_t k_max_candidates, rn_engine_t& rne) {
+	const float_t cell_size = min_dist / std::numbers::sqrt2_v<float_t>;
 
-	const float cell_size = min_dist / std::numbers::sqrt2_v<float>;
-    auto grid_w = std::lround(float(width) / cell_size);
-    auto grid_h = std::lround(float(height) / cell_size);
+    _grid_t<point2d_t> grid(width, height, cell_size);
 
-    point_t first_point(rng(rne) * width, rng(rne) * height);
+    typedef std::vector<point2d_t> points_t;
+    points_t active;
+    points_t generated;
+    generated.reserve(grid.w * grid.h);
 
-    std::vector<point_t> generated;
-    generated.reserve(grid_w * grid_h);
-    generated.push_back(first_point);
+	std::uniform_real_distribution<float_t> rng;
+    point2d_t first(rng(rne) * (width - 1), rng(rne) * (height - 1));
+    
+    grid.add_point(first);
+	active.push_back(first);
+    generated.push_back(first);
 
-    _grid_t<point_t> grid(cell_size, grid_w, grid_h);
-    grid.add_point(first_point);
+	int steps = 0;
+    const auto start = std::chrono::steady_clock::now();
+    while (!active.empty()) {
+        ++steps;
 
-    std::vector<point_t> active_list;
-	active_list.push_back(first_point);
+        std::uniform_int_distribution<typename points_t::size_type> idx_rng(0, active.size() - 1);
+        typename points_t::size_type random_idx = idx_rng(rne);
+        point2d_t point = active[random_idx];
 
-	int tries = 0;
-    while (!active_list.empty()) {
-        point_t active_point = _remove_random(active_list, rne);
+		bool found = false;
+        for (int_t i = 0; i < k_max_candidates; ++i) {
+			point2d_t candidate = _generate_random_point_around(point, min_dist, rne);
+			if (!_is_valid_candidate(candidate, grid, min_dist))
+                continue;
 
-		for (uint8_t i = 0; i < k_max_point_count; ++i) {
-			point_t candidate = _generate_random_point_around(active_point, min_dist, rne);
-			if (_is_valid_candidate(candidate, grid, min_dist)) {
-                generated.push_back(candidate);
-                grid.add_point(candidate);
-                active_list.push_back(candidate);
-            }
+            grid.add_point(candidate);
+            active.push_back(candidate);
+            generated.push_back(candidate);
+
+            found = true;
+            break;
 		}
 
-        std::cout << "after try " << tries << ": " << active_list.size() << std::endl;
-        ++tries;
-        if (tries > 20)
-            break;
+        if (!found) {
+            _remove_at(active, random_idx);
+        }
 	}
+    const auto end = std::chrono::steady_clock::now();
+    
+    std::cout << "converged in " << steps << " steps" << std::endl;
+    std::cout << "generated " << generated.size() << " points in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
 
 	return generated;
 }
