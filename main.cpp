9754969ba575bb7f25316231fed42ef06728aea5
dsecrieru <51512828+dsecrieru@users.noreply.github.com>
2023-03-21 21:43:37 +0100

    Refinements (#3)
    * refinements

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <chrono>
+#include <iostream>
+
 #include "pds_Bridson.hpp"
 
 template<typename T>
@@ -11,12 +14,17 @@ struct point2d {
 int main(int argc, char* argv[]) {
 	std::mt19937 rne;
 
-	auto bridsonSamples = blue_noise::pds_Bridson_sampling_2d<point2d<float>, std::mt19937>(
-            100, 100,
-            10,
+    const auto start = std::chrono::steady_clock::now();
+    auto bridsonSamples = blue_noise::pds_Bridson_sampling_2d<point2d<float>, std::mt19937>(
+            1600, 900,
+            9.6f,
             30,
             rne
     );
+    const auto end = std::chrono::steady_clock::now();
+    std::cout << "generated: " << bridsonSamples.size()
+        << " in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms"
+        << std::endl;
 
 	return 0;
 }
